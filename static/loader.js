76436c021eb221673958fa22a9eea2eeafca42a5
iskandaryv <iskandaryv@mail.ru>
2026-06-04 08:54:36 +0300

    Upload menu item: file icon (red) instead of gear, bold italic label for visibility

--- static/loader.js
+++ static/loader.js
@@ -167,6 +167,22 @@
         done = true;
       }
     }
+    // swap the gear icon for a FILE icon (replace the cloned <svg>'s inner paths)
+    var svg = clone.querySelector("svg");
+    if (svg) {
+      svg.setAttribute("viewBox", "0 0 24 24");
+      svg.setAttribute("fill", "none");
+      svg.setAttribute("stroke", "currentColor");
+      svg.setAttribute("stroke-width", "2");
+      svg.setAttribute("stroke-linecap", "round");
+      svg.setAttribute("stroke-linejoin", "round");
+      // document/file glyph
+      svg.innerHTML =
+        '<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>' +
+        '<polyline points="14 2 14 8 20 8"></polyline>' +
+        '<line x1="12" y1="18" x2="12" y2="12"></line>' +
+        '<polyline points="9 15 12 12 15 15"></polyline>';
+    }
     // open the uploader on the same host:8810 in a new tab
     var host = location.hostname || "localhost";
     var url = location.protocol + "//" + host + ":8810/";
